d5d42f8bda004e6a18855637cb6687d358459872
RomanK <rk@staffcop.ru>
2018-04-03 12:14:37 +0700

    example config

---
+++ config_example.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  config_example.py
+
+login = 'admin_login'
+password = 'admin_password'
+host = 'ip or dns from your miktotik'
+token = 'token your bot'
+bot_admin_password = 'password from first authorization admin-user in bot'
